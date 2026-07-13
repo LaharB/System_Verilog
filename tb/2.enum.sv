027d2c6addce97cb9e870e9c93330301da2c39ec
LaharB <laharbaruah96@gmail.com>
2026-07-13 20:50:44 +0530

    Code Files updated

--- tb/2.enum.sv
+++ tb/2.enum.sv
@@ -25,3 +25,21 @@ module enum_typedef_example;
   end
   
 endmodule
+
+typedef enum logic[2:0] {
+  IDLE = 3'b000, 
+  READ = 3'b001, 
+  WRITE = 3'b010,
+  ERROR = 3'b111
+} state_t; //custom datatype: logic and size: 3-bits 
+
+module state_enum;
+  
+  state_t current_state;
+  
+  initial begin
+    current_state = READ;
+    $display("Current state:%s(%0d)", current_state.name(), current_state);   
+  end
+  
+endmodule
