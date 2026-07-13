e5eaa48f24e23adc286d69bde1613fcbb2b833f5
LaharB <laharbaruah96@gmail.com>
2026-07-13 21:49:31 +0530

    Files updated

--- tb/5.fork_join.sv
+++ tb/5.fork_join.sv
@@ -27,3 +27,60 @@ module fork_join;
   
 endmodule
 
+module fork_join_any;
+  
+  initial begin
+    $display("------------------------------");
+    
+    fork
+      //Process 1
+      begin
+        $display($time,"\t Process-1 Started");
+        #5;
+        $display($time,"\t Process-1 Finished"); 
+      end
+      
+      //Process 2
+      begin
+        $display($time,"\t Process-2 Started");
+        #20;
+        $display($time,"\t Process-2 Finished"); 
+      end
+    join
+    
+    $display($time,"\t Outside Fork-Join");
+    $display("------------------------------");
+    #100 $finish;
+    
+  end     
+  
+endmodule
+
+module fork_join_any;
+  
+  initial begin
+    $display("------------------------------");
+    
+    fork
+      //Process 1
+      begin
+        $display($time,"\t Process-1 Started");
+        #5;
+        $display($time,"\t Process-1 Finished"); 
+      end
+      
+      //Process 2
+      begin
+        $display($time,"\t Process-2 Started");
+        #20;
+        $display($time,"\t Process-2 Finished"); 
+      end
+    join_any
+    
+    $display($time,"\t Outside Fork-Join");
+    $display("------------------------------");
+    #100 $finish;
+    
+  end     
+  
+endmodule
