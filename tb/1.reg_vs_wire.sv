a97cdb501495ff7ab2def0b3308008ba3d85297d
LaharB <laharbaruah96@gmail.com>
2026-07-13 20:33:15 +0530

    Code Files updated

---
+++ tb/1.reg_vs_wire.sv
@@ -0,0 +1,14 @@
+module example_verilog_bad(
+  input a,
+  input b,
+  output wire out
+);
+  
+  reg my_signal; //reg type 
+  
+  assign my_signal = a & b; //cannot assign wire to a reg - ERROR
+  
+  assign out = my_signal; //can assign both reg and wire to a net datatype 
+  
+endmodule
+
