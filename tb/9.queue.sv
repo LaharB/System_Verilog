3661f24c65d8dd87183a3567fcca0229eaac1d6b
LaharB <laharbaruah96@gmail.com>
2026-07-14 19:50:06 +0530

    Files updated

--- tb/9.queue.sv
+++ tb/9.queue.sv
@@ -23,3 +23,47 @@ module queue_example;
   end 
 
 endmodule
+
+module queue_datatypes_simple;
+  
+  int int_q[$] = '{10, 20, 30};
+  bit[3:0] bit_q[$] = '{5, 0, 7, 11};
+  real real_q[$] = '{1.5, 2.75, 3.0};
+  string str_q[$] = '{"apple", "banana", "cherry"};
+  
+  //enum queue
+  typedef enum {RED, GREEN, BLUE} color_t;
+  color_t color_q[$] = '{RED, GREEN, BLUE};
+  
+  initial begin
+    $display("Interger Queue: %p", int_q);
+    $display("Bit Queue: %p", bit_q);
+    $display("Real Queue: %p", real_q);
+    $display("String Queue: %p", str_q);
+    $display("Enum Queue: %p", color_q);
+  end
+
+endmodule
+
+// Bounded and Unbounded Queue 
+
+module queue_unbounded_example;
+  
+  int unbounded_q[$];
+  int bounded_q[$:4]; 
+  
+  initial begin
+    for(int i = 1; i <= 6; i++) begin
+      unbounded_q.push_back(i);
+      if(bounded_q.size() < 4)
+        bounded_q.push_back(i);
+      else
+        $display("Bounded Queue Full! Cannot add %0d", i);
+    end
+    
+    //display both the queues
+    $display("Unbounded Queue: %p", unbounded_q);
+    $display("Bounded Queue: %p", bounded_q);
+  end
+
+endmodule
