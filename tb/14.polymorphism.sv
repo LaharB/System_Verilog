9ccc9952f8135837070ebbdcc8afb9cb4eade92f
LaharB <laharbaruah96@gmail.com>
2026-07-16 00:40:28 +0530

    Files updated

--- tb/14.polymorphism.sv
+++ tb/14.polymorphism.sv
@@ -93,5 +93,58 @@ module tb;
   
 endmodule
 
+////////////////////////////////////////////////////////////////////
 
+// Example 3
+
+class parent;
+  
+  virtual task display();
+    $display("PARENT: display task");
+  endtask 
+    
+  task common_task(); //virtual not used so parent handle will call this task
+    $display("PARENT: common task" );   
+  endtask
+  
+endclass
+
+class child extends parent;
+  
+  task display();
+    $display("CHILD: display task");
+  endtask 
+  
+  task child_only_task();
+    $display("CHILD: special task");    
+  endtask
+  
+endclass
+  
+module tb;
+  
+  parent p;
+  child c;
+  
+  initial begin
+    c = new();
+    
+    p = c; //class assignment 
+    
+    /*
+    c = p;
+    This will result in a compilation error. A child handle cannot point to a parent object.
+
+Even if you try to force it dynamically using $cast(c, p);, the cast will fail at runtime. SystemVerilog strictly prevents a child handle from pointing to a base parent object because the parent object lacks the properties and methods (like child_only_task()) that the child handle expects to be able to access.
+	*/
+    
+    p.display(); //will call Child's Function
+    
+    p.common_task(); //calls Parent's common task not child's common task
+    
+    c.child_only_task(); //cant call this using p handle 
+    
+  end
+  
+endmodule
 
