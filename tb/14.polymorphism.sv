25f4d0d3ebb80a0cd33ca6968ad343b5521a537c
LaharB <laharbaruah96@gmail.com>
2026-07-16 00:40:10 +0530

    Files updated

--- tb/14.polymorphism.sv
+++ tb/14.polymorphism.sv
@@ -47,3 +47,51 @@ endmodule
 
 // Example 2
 
+class parent;
+  
+  virtual function void display();
+    $display("Inside PARENT CLASS");
+  endfunction  
+  
+endclass
+
+class child_1 extends parent;
+  
+  function void display();
+    $display("Inside CHILD_1 CLASS");
+  endfunction  
+  
+endclass
+
+class child_2 extends parent;
+  
+  function void display();
+    $display("Inside CHILD_2 CLASS");
+  endfunction  
+  
+endclass
+  
+module tb;
+  
+  parent p[2]; //array of handles 
+  child_1 c1;
+  child_2 c2;
+  
+  initial begin
+    //p[2] = new();
+    c1 = new();
+    c2 = new();
+    
+    //class assignment 
+    p[0] = c1;
+    p[1] = c2;
+    
+    p[0].display();
+    p[1].display();
+    
+  end
+  
+endmodule
+
+
+
