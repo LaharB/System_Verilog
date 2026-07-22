8a6e3b04a79c835087a42cd3f6b718cdea6dbd18
LaharB <laharbaruah96@gmail.com>
2026-07-22 22:59:59 +0530

    Files updated

--- tb/6.static_arrays.sv
+++ tb/6.static_arrays.sv
@@ -11,3 +11,17 @@ module array_example;
   end
   
 endmodule
+
+//2D array
+module array_example;
+  
+  int array[6][2] = '{ '{10, 100}, '{20, 200}, '{30, 300}, '{40, 400}, '{50, 500}, '{60, 600} }; //0th row elements, 1st row elemets and so on 
+  initial begin
+    foreach(array[i, j]) begin
+      $display("array[%0d][%0d] is %0d", i, j, array[i][j]);   
+    end
+    
+  end
+  
+  
+endmodule
